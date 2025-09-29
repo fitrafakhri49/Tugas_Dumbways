f500a0966e27414667a4e63093c2ad814e1155c8
Fakhri Fitra <fitrafakhri49@gmail.com>
2025-09-29 10:12:27 +0700

    finalisasi

---
+++ assets/scripts/index.js
@@ -0,0 +1,14 @@
+for (let i = 1; i <= 4; i++) {
+  document.write(`
+    <div class="card" style="width: 18rem;">
+      <img src="assets/images/tech2.jpeg" class="card-img-top" alt="Gambar tech">
+      <div class="card-body">
+        <a href="Details.html"><h5 class="card-title">Dumbways Project- 2021</h5></a>
+        <p class="card-text">durasi: 3 bulan</p>
+        <p class="card-text">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+        <a href="#" class="btn btn-primary">edit</a>
+        <a href="#" class="btn btn-danger">delete</a>
+      </div>
+    </div>
+  `);
+}
